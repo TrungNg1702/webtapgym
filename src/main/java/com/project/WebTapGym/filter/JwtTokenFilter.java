c66b86b77b6d6bd65c0c7246f9515ef5f0cf93d9
TrungBoy <trungboy1122@gmail.com>
2025-06-12 10:39:34 +0700

    Them Filter

--- src/main/java/com/project/WebTapGym/filter/JwtTokenFilter.java
+++ src/main/java/com/project/WebTapGym/filter/JwtTokenFilter.java
@@ -131,7 +131,10 @@ public class JwtTokenFilter extends OncePerRequestFilter {
                 Pair.of(String.format("%s/chatbot", apiPrefix), "POST"),
                 Pair.of(String.format("%s/food-analysis/analyze", apiPrefix), "POST"),
                 Pair.of(String.format("%s/workout-schedules", apiPrefix), "POST"),
-                Pair.of(String.format("%s/excel/upload", apiPrefix), "POST")
+                Pair.of(String.format("%s/excel/upload", apiPrefix), "POST"),
+                Pair.of(String.format("%s/excel/user/", apiPrefix), "GET"),
+                Pair.of(String.format("%s/excel/", apiPrefix), "POST"),
+                Pair.of(String.format("%s/excel/suggest", apiPrefix), "GET")
         );
 
         String requestPath = request.getServletPath();
